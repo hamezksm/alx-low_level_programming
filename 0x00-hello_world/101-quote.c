91a5d5df9a7945d984920a3db241a669a905bf05
hamezksm <jamcav097@gmail.com>
2023-02-16 15:14:27 -0800

    Number 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,8 +1,12 @@
 #include <unistd.h>
 
+/**
+ * main - C program that prints exactly what is written without printf and puts
+ * return: 1(Success)
+ */
 int main(void)
 {
 	char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 	write(2, message, 59);
-	return(1);
+	return (1);
 }

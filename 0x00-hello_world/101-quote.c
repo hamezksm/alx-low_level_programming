ab6e88b0aef1066b956bc2a11b6319ce4fadd607
hamezksm <jamcav097@gmail.com>
2023-02-16 16:12:57 -0800

    Number 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -10,5 +10,6 @@ int main(void)
 {
 	char *msg = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 	write(STDERR_FILENO, msg, strlen(msg));
+	write(STDERR_FILENO, "\n", 1);
 	return (1);
 }

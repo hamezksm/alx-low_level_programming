0337ad84eb97d8f5b977367cb36f16f09b76b2ae
hamezksm <jamcav097@gmail.com>
2023-03-14 14:33:32 -0700

    Correction on 101

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -11,6 +11,7 @@
 int count_word(char *s)
 {
 	int flag, c, w;
+
 	flag = 0;
 	w = 0;
 	for (c = 0; s[c] != '\0'; c++)
@@ -37,6 +38,7 @@ char **strtow(char *str)
 {
 	char **matrix, *tmp;
 	int i, k = 0, len = 0, words, c = 0, start, end;
+
 	while (*(str + len))
 		len++;
 	words = count_word(str);
